dbb946821f3739ba7e3363a6e61d95fa651d6d2d
Sergej <trpo@gmail.com>
2019-11-22 15:26:49 +0300

    Lab10

---
+++ 374_Bezuglyi_l10/SQL_l10/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_l10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'g374_l10_BezuglyiDataSet.planets' table. You can move, or remove it, as needed.
+            this.planetsTableAdapter.Fill(this.g374_l10_BezuglyiDataSet.planets);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            planetsTableAdapter.Update(g374_l10_BezuglyiDataSet);
+        }
+    }
+}
